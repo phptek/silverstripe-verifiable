0f2e49e2705cb06c2dfe6e4235226b64be09f225
Russell Michell <russ@theruss.com>
2018-06-23 14:18:02 +1200

    Added skeleton "gateway" logic.

---
+++ src/Verify/Verifiable.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @author  Russell MIchell 2018 <russ@theruss.com>
+ * @package silverstripe-verifiable
+ */
+
+namespace PhpTek\Verifiable\Verify;
+
+use SilverStripe\Core\DataExtension;
+
+/**
+ * All applicable database writes are passed through here.
+ *
+ */
+class VerifiableExtension extends DataExtension
+{
+}
+
