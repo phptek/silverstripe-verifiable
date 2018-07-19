dd70b4bbb82df2d7a59fe1b21d0bda425a83c980
Russsell Michell <russ@theruss.com>
2018-07-19 22:07:09 +1200

    FIX: Fixes #9 Refactored backend logic into separate service+gateway implementation

---
+++ src/Backend/ServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @author  Russell Michell 2018 <russ@theruss.com>
+ * @package silverstripe-verifiable
+ */
+
+namespace PhpTek\Verifiable\Backend;
+
+/**
+ * Defines exactly what services should look like.
+ */
+interface ServiceProvider
+{
+    /**
+     * @return string
+     */
+    public function name() : string;
+    
+}
