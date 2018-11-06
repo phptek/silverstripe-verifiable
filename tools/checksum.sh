91aaaf45fbbbf22dc6f85ec5e7f592f942dea956
Russell Michell <russ@theruss.com>
2018-11-06 21:38:53 +1300

    NEW: Fixes #27 Basic checksum capability

---
+++ tools/checksum.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function do_checksum ()
+{
+  tar -c \
+        --sort=name \
+        --owner=0 \
+        --group=0 \
+        --mode=755 \
+        --numeric-owner \
+        --mtime="2018-11-06T21:45:07,354730315+13:00" \
+        --clamp-mtime \
+        --exclude-vcs \
+        --no-ignore-case \
+        --exclude=CHECKSUM . | sha256sum | sed -e 's#[\s\-]##g'
+}
+
+if [ ! -z "${1}" ]; then
+  do_checksum
+fi
